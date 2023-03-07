06d634a7157b582333bb6ede3b9b430251364d31
Roqibul19 <haque15-5034@diu.edu.bd>
2023-03-08 04:07:07 +0600

    added last card

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-// fetch loads
+// fetch to load all card
 const fetchItems= (limit)=>{
     fetch('https://openapi.programming-hero.com/api/ai/tools')
     .then(res=> res.json())
     .then(data=>seeMore(limit,data.data.tools));
 }
 
-// for see more
+// for see more cards
 const seeMore=(limit,data)=>{
     if(limit===6){
         data=data.slice(0,6);
@@ -58,22 +58,128 @@ const displayFetchData=(data)=>{
 // calling for 6 cards only
 fetchItems(6);
 
-// see more cards
+// see more cards button
 const allCardButton= document.getElementById('see-more').addEventListener('click',function(){
     fetchItems();
     document.getElementById('see-more').classList.add("d-none");
  })
 
-//  more info in modal
+  // more info in modal
  
-const openModal=(id)=>{
-     url=`https://openapi.programming-hero.com/api/ai/tool/${id}`
-     fetch(url)
-     .then(res=> res.json())
-     .then(data=>ShowModalInfo(data.data))
+ const openModal=(id)=>{
+    const url=`https://openapi.programming-hero.com/api/ai/tool/${id}`
+    fetch(url)
+     .then(res=>res.json())
+     .then(data=>ShowModalInfo(data.data,id));
+  }
      
-const ShowModalInfo=(data)=>{
-         
-    }}
+const ShowModalInfo=(data,id)=>{
+   if(id==='12'){
+    lastCardModalShow(data);
+   }
+   else{
+    allcardModalShow(data);
+   }
 
+}
+const lastCardModalShow=(data)=>{
+  const modalContainer= document.getElementById('modal-container');
+  modalContainer.innerHTML='';
+  const modalDiv= document.createElement('div');
+  modalDiv.classList.add('modal-content');
+  modalDiv.innerHTML=`
+   <div class="modal-header">
+  <div class="d-sm-flex">
+      <div class="border border-danger bg-warning-subtle w-50 p-3">
+        <h5>${data.description? data.description:"No"}</h5>
+        <div class="d-flex justify-content-evenly m-2">
+          <p class="p-2 fw-bold text-danger">Free of<br>cost/<br>Basic</p>
+          <p class="p-2 fw-bold text-success">Free of<br>cost/<br>Pro</p>
+          <p class="p-2 fw-bold text-primary">Free of<br>cost/<br>Enterprise</p>
+          
+        </div>
+        <div class="d-flex justify-content-between">
+           <div>
+             <h6 class="m-2">Features</h6>
+           <ol>
+             <li>${data.features['1'].feature_name?data.features['1'].feature_name:"No data found"}</li>
+             <li>${data.features['2'].feature_name?data.features['2'].feature_name:"No data found"}</li>
+             <li>${data.features['3'].feature_name?data.features['3'].feature_name:"No data found"}</li>
+             <li>${data.features['4'].feature_name?data.features['4'].feature_name:"No data found"}</li>
+           </ol>
+           </div>
+           <div>
+             <h6 class="m-2">Integrations</h6>
+           <p>No Data Found!</p>
+           </div>
+        </div>
+      </div>
+      <div class="w-50 p-2">
+        <img class="img-fluid m-2" src="${data.image_link[0]}" alt="">
+        <h6>Can you give any Example?</h6>
+        <p>No Not yet! Take a break</p>
+      </div>
+  </div>
+ <button type="button" class="btn-close bg-danger" data-bs-dismiss="modal" aria-label="Close"></button>
+</div>
+</div>
+</div>
 
+  
+  `
+  modalContainer.appendChild(modalDiv);
+}
+const allcardModalShow=(data)=>{
+  if(data.accuracy.score===null){
+    const modalContainer= document.getElementById('modal-container');
+    modalContainer.innerHTML='';
+    const modalDiv= document.createElement('div');
+    modalDiv.classList.add('modal-content');
+    modalDiv.innerHTML=`
+     <div class="modal-header">
+    <div class="d-sm-flex">
+        <div class="p-2 border border-danger bg-warning-subtle w-50">
+          <h5>${data.description? data.description:"No"}</h5>
+          <div class="d-flex justify-content-evenly m-2">
+            <p>Free of cost</p>
+            <p class="p-2">${data.pricing[1].price?data.pricing[1].price:"Free of Cost"}
+             ${data.pricing[1].plan}</p>
+            <p class="p-2">${data.pricing[2].price?data.pricing[2].price:"Free of Cost"}
+             ${data.pricing[2].plan}</p>
+            
+          </div>
+          <div class="d-flex justify-content-between">
+             <div>
+               <h6 class="m-2">Features</h6>
+             <ol>
+               <li>${data.features['1'].feature_name?data.features['1'].feature_name:"No data found"}</li>
+               <li>${data.features['2'].feature_name?data.features['2'].feature_name:"No data found"}</li>
+               <li>${data.features['3'].feature_name?data.features['3'].feature_name:"No data found"}</li>
+             </ol>
+             </div>
+             <div>
+               <h6 class="m-2">Integrations</h6>
+             <ol>
+               <li>${data.integrations[0]?data.integrations[0]:"No data found"}</li>
+             </ol>
+             </div>
+          </div>
+        </div>
+        <div class="w-50 p-2">
+          <img class="img-fluid m-2" src="${data.image_link[0]}" alt="">
+          <h6>${data.input_output_examples[0].input?data.input_output_examples[0].input:"No Not yet! Take a break!"}</h6>
+          <p>${data.input_output_examples[0].output?data.input_output_examples[0].output:"No Not yet! Take a break!"}</p>
+        </div>
+    </div>
+   <button type="button" class="btn-close bg-danger" data-bs-dismiss="modal" aria-label="Close"></button>
+  </div>
+  </div>
+  </div>
+  
+    
+    `
+    modalContainer.appendChild(modalDiv);
+  }
+  
+  
+  }
